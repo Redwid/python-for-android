e823fd5528343c0960534b69bb2706d93b38a84a
Redwid <redwid@gmail.com>
2018-05-21 11:23:42 +0100

    Refactor build script

--- clean-build-copy.sh
+++ clean-build-copy.sh
@@ -7,7 +7,6 @@ export PYTHONPATH=$PWD
 
 #script set up
 p4a="$PWD/pythonforandroid/toolchain.py"
-#pythonApp="~/Projects/git/youtube-dl/youtube_dl"
 pythonApp="$(dirname "$PWD")/youtube-dl/youtube_dl"
 pythonAppDistName="youtube_dl_wrapper"
 pythonAppPackageName="org.youtube.dl"
@@ -15,13 +14,25 @@ pythonAppName="youtube_dl"
 distFolder="$HOME/.python-for-android/dists/$pythonAppDistName"
 ndkDir=~/Library/Android/sdk/ndk-bundle
 
+#Link for android-youtube-dl project to where script will copy build artifacts
+androidYoutubeDlProject="$(dirname "$PWD")/android-youtube-dl"
+
 clean() {
+echo "[INFO]     Clean"
 /usr/bin/python $p4a clean_builds
 rm -rfv $distFolder
+rm -rfv $PWD/build
+echo "[INFO]     Clean done"
 }
 
 build() {
+echo "[INFO]     Build [$1]"
+/usr/bin/python $p4a clean_builds
+rm -rfv $distFolder
 /usr/bin/python $p4a apk --private $pythonApp --dist_name=$pythonAppDistName --package=$pythonAppPackageName --name=$pythonAppName --version=1 --ndk_dir $ndkDir --ndk_version r16b --requirements=android,pyopenssl,pycrypto,openssl --android_api=23 --arch=$1 --java-build-tool gradle
+copy_libs
+copy_assets
+echo "[INFO]     Build [$1] done"
 }
 
 copy_libs() {
@@ -32,23 +43,29 @@ cp -rfv $distFolder/libs/ $PWD/build/libs/
 copy_assets() {
 cp -rfv $distFolder/src/main/assets/ $PWD/build/assets
 rm $PWD/build/assets/.gitkeep
+
+delete_unused_assets "armeabi-v7a"
+delete_unused_assets "x86"
+delete_unused_assets "arm64-v8a"
 }
 
-echo "[INFO]     Clean"
-clean
-rm -rfv $PWD/build
-echo "[INFO]     Clean done"
+delete_unused_assets() {
+rm $PWD/build/libs/$1/libSDL2.so
+rm $PWD/build/libs/$1/libSDL2_image.so
+rm $PWD/build/libs/$1/libSDL2_mixer.so
+rm $PWD/build/libs/$1/libSDL2_ttf.so
+}
 
-echo "[INFO]     Build [arm64-v8a]"
-build "arm64-v8a"
-copy_libs
-copy_assets
-echo "[INFO]     Build [arm64-v8a] done"
+copy_assets_to_androidYoutubeDlProject() {
+cp -rfv $PWD/build/assets/ $androidYoutubeDlProject/lib/src/main/assets
+cp -rfv $PWD/build/libs/   $androidYoutubeDlProject/lib/src/main/jniLibs
+}
 
-echo "[INFO]     Build [armeabi-v7a]"
 clean
+
 build "armeabi-v7a"
-copy_libs
-echo "[INFO]     Build [armeabi-v7a] done"
+build "x86"
+build "arm64-v8a"
 
+copy_assets_to_androidYoutubeDlProject
 echo "[INFO]     Build done"
