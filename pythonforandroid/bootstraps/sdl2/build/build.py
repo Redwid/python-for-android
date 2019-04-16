c2f2218138899b2157c1cff6dc801b0bc2fe6267
Redwid <redwid@gmail.com>
2019-04-16 19:49:25 +0100

    Add debug output

--- pythonforandroid/bootstraps/sdl2/build/build.py
+++ pythonforandroid/bootstraps/sdl2/build/build.py
@@ -221,6 +221,7 @@ def make_package(args):
     # Ignore warning if the launcher is in args
     if not args.launcher:
         print('args.private path: {}'.format(args.private))
+        print('realpath: {}'.format(join(realpath(args.private), 'main.py')))
         if not (exists(join(realpath(args.private), 'main.py')) or
                 exists(join(realpath(args.private), 'main.pyo'))):
             print('''BUILD FAILURE: No main.py(o) found in your app directory. This
