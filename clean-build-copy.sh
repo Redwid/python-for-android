2d2973bac6de0a83c66e8120b928ee803d654ee4
Redwid <redwid@gmail.com>
2019-04-16 22:54:12 +0100

    Add method to crete aar files

--- clean-build-copy.sh
+++ clean-build-copy.sh
@@ -23,7 +23,7 @@ export PYTHONUNBUFFERED=1
 export VERSIONER_PYTHON_VERSION=2.7
 export PYTHONPATH=$PWD
 export ANDROIDAPI="23"
-export NDKAPI="16"
+export NDKAPI="23"
 
 if [ -z "$ANDROID_NDK_HOME" ]
 then
@@ -51,49 +51,89 @@ echo "distFolder: ${distFolder}"
 #Link for android-youtube-dl project to where script will copy build artifacts
 androidYoutubeDlProject="$(dirname "$PWD")/android-youtube-dl"
 
+
 clean() {
-echo "[INFO]     Clean"
-/usr/bin/python $p4a clean_builds
-rm -rfv $distFolder
-rm -rfv $PWD/build
-echo "[INFO]     Clean done"
+    echo "[INFO]     Clean"
+    /usr/bin/python $p4a clean_builds
+    rm -rfv $distFolder
+    rm -rfv $PWD/build
+    echo "[INFO]     Clean done"
 }
+
 
 build() {
-echo "[INFO]     Build [$1]"
-/usr/bin/python $p4a clean_builds
-rm -rfv $distFolder
-/usr/bin/python $p4a apk --private $pythonApp --dist_name=$pythonAppDistName --package=$pythonAppPackageName --name=$pythonAppName --version=1 --ndk_dir $ANDROID_NDK_HOME --ndk_version r16b --requirements=android,pyopenssl,pycrypto,openssl --android_api=23 --arch=$1 --java-build-tool gradle
-copy_libs
-copy_assets
-echo "[INFO]     Build [$1] done"
+    echo "[INFO]     Build [$1]"
+    python $p4a clean_builds
+    rm -rfv $distFolder
+    python $p4a apk --private $pythonApp --dist_name=$pythonAppDistName --package=$pythonAppPackageName --name=$pythonAppName --version=1 --ndk_dir $ANDROID_NDK_HOME --ndk_version r16b --requirements=android,pyopenssl,pycrypto,openssl --android_api=23 --arch=$1 --java-build-tool gradle
+#    if [ -z "$TRAVIS" ]
+#    then
+#        copy_libs
+#        copy_assets
+#    else
+        copy_assets_and_make_aar $1
+#    fi
+    echo "[INFO]     Build [$1] done"
 }
+
 
 copy_libs() {
-mkdir $PWD/build
-cp -rfv $distFolder/libs/ $PWD/build/libs/
+    mkdir $PWD/build
+    cp -rfv $distFolder/libs/ $PWD/build/libs/
 }
+
 
 copy_assets() {
-cp -rfv $distFolder/src/main/assets/ $PWD/build/assets
-rm $PWD/build/assets/.gitkeep
+    cp -rfv $distFolder/src/main/assets/ $PWD/build/assets
+    rm $PWD/build/assets/.gitkeep
 
-delete_unused_assets "armeabi-v7a"
-#delete_unused_assets "x86"
-delete_unused_assets "arm64-v8a"
+    delete_unused_assets "armeabi-v7a"
+    #delete_unused_assets "x86"
+    delete_unused_assets "arm64-v8a"
 }
+
 
 delete_unused_assets() {
-rm $PWD/build/libs/$1/libSDL2.so
-rm $PWD/build/libs/$1/libSDL2_image.so
-rm $PWD/build/libs/$1/libSDL2_mixer.so
-rm $PWD/build/libs/$1/libSDL2_ttf.so
+    rm $PWD/build/libs/$1/libSDL2.so
+    rm $PWD/build/libs/$1/libSDL2_image.so
+    rm $PWD/build/libs/$1/libSDL2_mixer.so
+    rm $PWD/build/libs/$1/libSDL2_ttf.so
 }
 
+
 copy_assets_to_androidYoutubeDlProject() {
-cp -rfv $PWD/build/assets/ $androidYoutubeDlProject/lib/src/main/assets
-cp -rfv $PWD/build/libs/   $androidYoutubeDlProject/lib/src/main/jniLibs
+    cp -rfv $PWD/build/assets/ $androidYoutubeDlProject/lib/src/main/assets
+    cp -rfv $PWD/build/libs/   $androidYoutubeDlProject/lib/src/main/jniLibs
 }
+
+
+copy_assets_and_make_aar() {
+    echo "[INFO]     Copy assets [$1]"
+    #Copy *.so files
+    mkdir $PWD/build
+    mkdir $PWD/build/$1
+    mkdir $PWD/build/$1/jni
+    mkdir $PWD/build/$1/jni/$1
+    cp -rfv $distFolder/libs/$1 $PWD/build/$1/jni
+    rm $PWD/build/$1/jni/$1/libSDL2.so
+    rm $PWD/build/$1/jni/$1/libSDL2_image.so
+    rm $PWD/build/$1/jni/$1/libSDL2_mixer.so
+    rm $PWD/build/$1/jni/$1/libSDL2_ttf.so
+
+    #Copy *.mp3 file
+    mkdir $PWD/build/$1/assets
+    cp -rfv $distFolder/src/main/assets/private.mp3 $PWD/build/$1/assets/private-$1.mp3
+    echo "[INFO]     Copy assets [$1] done"
+
+    echo "[INFO]     Make aar [$1]"
+    #Create aar file
+    root=$PWD
+    cd $root/build/$1
+    zip $root/build/$1.aar -r ./*
+    cd $root
+    echo "[INFO]     Make aar [$1] done"
+}
+
 
 clean
 
@@ -101,5 +141,5 @@ build "armeabi-v7a"
 #build "x86"
 build "arm64-v8a"
 
-copy_assets_to_androidYoutubeDlProject
+#copy_assets_to_androidYoutubeDlProject
 echo "[INFO]     Build done"
