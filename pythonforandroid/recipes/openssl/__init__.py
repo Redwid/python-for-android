d509394fa048bc157c2a5e97d8ddc481cba26fd9
Redwid <redwid@gmail.com>
2020-04-08 13:01:45 +0100

    Replace url

--- pythonforandroid/recipes/openssl/__init__.py
+++ pythonforandroid/recipes/openssl/__init__.py
@@ -1,12 +1,14 @@
 from functools import partial
 
-from pythonforandroid.toolchain import Recipe, shprint, current_directory
 import sh
+
+from pythonforandroid.toolchain import Recipe, shprint, current_directory
 
 
 class OpenSSLRecipe(Recipe):
     version = '1.0.2h'
-    url = 'https://www.openssl.org/source/openssl-{version}.tar.gz'
+    #url = 'https://www.openssl.org/source/openssl-{version}.tar.gz'
+    url = 'https://github.com/openssl/openssl/archive/OpenSSL_1_0_2n.tar.gz'
 
     def should_build(self, arch):
         return not self.has_libs(arch, 'libssl' + self.version + '.so',
